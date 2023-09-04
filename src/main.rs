5cafc8570db567bd3da917164b0326c2992c5ce4
aimericsr <52284219+aimericsr@users.noreply.github.com>
2023-09-04 18:39:10 +0200

    login + cookie

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,57 @@
+pub use self::error::{Error, Result};
+
 use std::fmt::format;
 use std::net::SocketAddr;
 
-use axum::extract::Query;
+use axum::extract::{Path, Query};
+use axum::middleware;
+use axum::response::Response;
+use axum::routing::get_service;
 use axum::{
     response::{Html, IntoResponse},
     routing::get,
     Router,
 };
 use serde::Deserialize;
+use tower_cookies::CookieManagerLayer;
+use tower_http::services::ServeDir;
+
+mod error;
+mod model;
+mod web;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper))
+        .layer(CookieManagerLayer::new())
+        .fallback_service(routes_static());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    println!();
+    res
+}
+
+// use the tower ecosystem for the filesystem API
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
 }
 
 #[derive(Debug, Deserialize)]
@@ -29,4 +62,10 @@ struct HelloParams {
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     let name = params.name.as_deref().unwrap_or("World");
     Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello2 <strong>{name}</strong>"))
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,10 +1,22 @@
 use anyhow::Result;
+use serde_json::json;
 
 #[tokio::test]
 async fn main() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/hello?name=aimeric").await?.print().await?;
+    hc.do_get("/src/main.rs").await?.print().await?;
+
+    hc.do_get("/hello2/Mike").await?.print().await?;
+
+    let req_login = hc.do_post(
+        "/api/login",
+        json!({
+            "username": "demo1",
+            "pwd": "welcome"
+        }),
+    );
+    req_login.await?.print().await?;
 
     Ok(())
 }
