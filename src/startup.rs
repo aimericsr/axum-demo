09e8078e20bef7e74c6307d76bb40c59ac3649ee
aimericsr <aimeric.sorin@gmail.com>
2023-09-26 11:44:01 +0200

    add request id to each http request

--- src/startup.rs
+++ src/startup.rs
@@ -15,21 +15,24 @@ use crate::web::rest::routes_prometheus::routes as routes_prometheus;
 use crate::web::rest::routes_static::serve_dir as routes_static;
 use crate::web::routes_docs::routes as routes_docs;
 use crate::web::rpc;
+use axum::body::Body;
 use axum::http::HeaderValue;
 use axum::http::Method;
-use tokio::signal;
-use tower_http::timeout::TimeoutLayer;
-use tracing::Level;
-
-use axum::middleware;
+use axum::http::Request;
+use axum::middleware::{from_fn, from_fn_with_state, map_response};
 use axum::Router;
 use axum_tracing_opentelemetry::middleware::OtelAxumLayer;
 use axum_tracing_opentelemetry::middleware::OtelInResponseLayer;
 use std::net::SocketAddr;
 use std::time::Duration;
+use tokio::signal;
 use tower_cookies::CookieManagerLayer;
 use tower_http::cors::CorsLayer;
+use tower_http::timeout::TimeoutLayer;
+use tower_http::trace::TraceLayer;
+use tower_request_id::{RequestId, RequestIdLayer};
 use tracing::info;
+use tracing::info_span;
 
 pub async fn build() -> Result<()> {
     get_subscriber();
@@ -52,7 +55,7 @@ pub async fn build() -> Result<()> {
 }
 
 fn routes(mm: ModelManager) -> Router {
-    let routes_rpc = rpc::routes(mm.clone()).route_layer(middleware::from_fn(mw_ctx_require));
+    let routes_rpc = rpc::routes(mm.clone()).route_layer(from_fn(mw_ctx_require));
 
     let routes_all = Router::new()
         .merge(routes_docs())
@@ -61,13 +64,21 @@ fn routes(mm: ModelManager) -> Router {
         .merge(routes_health())
         .merge(routes_prometheus())
         .nest("/api", routes_rpc)
-        .layer(middleware::map_response(mw_res_map))
+        .layer(map_response(mw_res_map))
         // above CookieManagerLayer because we need it
-        .layer(middleware::from_fn_with_state(
-            mm.clone(),
-            web::mw_auth::mw_ctx_resolve,
-        ))
+        .layer(from_fn_with_state(mm.clone(), web::mw_auth::mw_ctx_resolve))
         .layer(CookieManagerLayer::new())
+        .layer(
+            TraceLayer::new_for_http().make_span_with(|request: &Request<Body>| {
+                let request_id = request
+                    .extensions()
+                    .get::<RequestId>()
+                    .map(ToString::to_string)
+                    .unwrap_or_else(|| "unknown".into());
+                info_span!("request_id", request_id)
+            }),
+        )
+        .layer(RequestIdLayer)
         // include trace context as header into the response
         .layer(OtelInResponseLayer::default())
         //start OpenTelemetry trace on incoming request
